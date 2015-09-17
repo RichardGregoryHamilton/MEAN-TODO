2704302673d04ca612b250449e35861e67e7fec6
Richard Hamilton <richardgregoryhamilton@gmail.com>
2015-09-17 15:58:39 -0500

    Separate code into multiple files

--- server.js
+++ server.js
@@ -1,11 +1,10 @@
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
+var port = process.env.PORT || 8080;
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
-
-mongoose.connect('mongodb://node:nodeuser@mongo.onmodulus.net:27017/uw03mypu');
 
 app.use(express.static(__dirname + '/public'));
 app.use(morgan('dev'));
@@ -14,52 +13,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride());
 
+var database = require('./config/database');
+mongoose.connect(database.url);
+
+require('./app/routes')(app);
+
 app.listen(8080);
 console.log("App listening on port 8080");
-
-var Todo = mongoose.model('Todo', {
-    text: String
-});
-
-app.get('/api/todos', function(req, res) {
-    Todo.find(function(err, todos) {
-        if (err) {
-            res.send(error)
-        }
-        res.json(todos);
-    });
-});
-
-app.post('/api/todos', function(req, res) {
-    Todo.create({text: req.body.text, done: false}, function(err, todo) {
-        if (err) {
-            res.send(err);
-        }
-        Todo.find(function(err,todos) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(todos);
-        });
-    });
-});
-
-app.delete('/api/todos', function(req, res) {
-    Todo.remove({
-        _id: req.params.todo_id
-    }, function(err, todo) {
-        if (err) {
-            res.send(err);
-        }
-        Todo.find(function(err,todos) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(todos);
-        });
-    });
-});
-
-app.get('*', function(req,res) {
-    res.sendFile('./public/index.html');
-});
